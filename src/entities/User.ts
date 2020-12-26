591f3d6381a8e478bacf5d866df1df7613b78806
adam <adamjleung123@gmail.com>
2020-12-26 22:25:01 +1100

    Changed schema, set up basic github authentication

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -10,18 +10,49 @@ import Activity from "./Activity";
 
 @Entity()
 export default class User extends BaseEntity {
+  /**
+   * The unique user id for each user
+   */
   @PrimaryGeneratedColumn("uuid")
   id!: number;
 
+  /**
+   * The unique github id associated with the user's github account
+   */
   @Column("varchar", { unique: true })
   githubId!: string;
 
+  /**
+   * The access token for the user's github profile
+   * Note: Access tokens for githubs api does not expire until they are
+   * revoked by the user, hence we can simply store the token in the
+   * database without needing to use refresh tokens to generate new access
+   * tokens everytime
+   */
   @Column("text")
-  displayName!: string;
+  accessToken!: string;
 
+  /**
+   * The user's github username. These are unique
+   */
+  @Column("varchar", { unique: true })
+  username!: string;
+
+  /**
+   * The display name of the user. May not be set on github!
+   */
+  @Column("text", { nullable: true })
+  displayName!: string | null;
+
+  /**
+   * Url to the user's github profile pic
+   */
   @Column("text")
   profilePicUrl!: string;
 
+  /**
+   * Latest activity associated with the user
+   */
   @OneToOne(() => Activity, (activity) => activity.owner)
   @JoinColumn()
   activity!: Activity;
--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,11 @@ import passport from "passport";
 import { Strategy as GitHubStrategy } from "passport-github";
 import * as dotenv from "dotenv";
 import "reflect-metadata";
-import { createConnection } from "typeorm";
+import { createConnection, getConnection } from "typeorm";
 import User from "./entities/User";
 import Activity from "./entities/Activity";
+
+const PORT = 8000;
 
 const main = async () => {
   const key = fs.readFileSync("./key.pem");
@@ -15,12 +17,14 @@ const main = async () => {
   dotenv.config(); // Configure reading of .env file
 
   const app = express();
-  const PORT = 8000;
-
   const server = https.createServer({ key, cert }, app);
 
   // Setup the connection to the database
   const db = await createConnection().catch((e) => console.log(e));
+  if (!db) {
+    console.log("Database connection error");
+    return;
+  }
 
   // Setup the passport strategy for github auth
   passport.use(
@@ -28,21 +32,67 @@ const main = async () => {
       {
         clientID: process.env.GITHUB_CLIENT_ID,
         clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        callbackURL: `${process.env.URL}/callback`,
+        callbackURL: `${process.env.URL}/auth/callback`,
       },
-      (accessToken, refreshToken, profile, callback) => {
-        // callback(Error, "b");
+      async (accessToken, refreshToken, profile, callback) => {
+        try {
+          /**
+           *  Either find the existing user matching the github profile id
+           *  or create a new user with the given information from the profile
+           *  Note, refreshToken is undefined!
+           */
+          let user: User | undefined = await User.findOne({
+            githubId: profile.id,
+          });
+          const userData = {
+            githubId: profile.id,
+            accessToken,
+            username: profile.username,
+            displayName: profile.displayName,
+            profilePicUrl: profile.photos?.[0] ? profile.photos?.[0].value : "",
+          };
+
+          if (user) {
+            await User.update(user.id, userData);
+          } else {
+            user = await User.create(userData).save();
+          }
+
+          callback(null, user);
+        } catch (e) {
+          console.log(e);
+          callback(new Error("Internal Server Error"));
+        }
       }
     )
   );
 
+  passport.serializeUser((user: User, done) => {
+    done(null, user.id);
+  });
+
+  app.use(passport.initialize());
+
   app.get("/", (req, res) => res.send("Express Server Running"));
 
+  app.get("/failed", (req, res) => res.send("Authentication failed :("));
+
+  app.get("/done", (req, res) => res.send("You are done authenticating!"));
+
   app.get(
-    "/callback",
-    passport.authenticate("github", { failureRedirect: "/login" }),
+    "/auth",
+    passport.authenticate("github", { session: false, scope: ["read:user"] })
+  );
+
+  app.get(
+    "/auth/callback",
+    passport.authenticate("github", { failureRedirect: "/failed" }),
     (req, res) => {
-      res.redirect("/");
+      console.log("req is:");
+      console.log(req);
+      console.log("req is:");
+      console.log(res);
+      res.redirect("/done");
     }
   );
 
